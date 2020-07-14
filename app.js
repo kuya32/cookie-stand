1445bd7470db3c068a27297e75af9a064896bb18
Marchael Acode <m.acode@outlook.com>
2020-07-14 14:27:05 -0700

    Added 3 object literals

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var seattle = {
   maxCustomers: 65,
   avgCookiePerCust: 6.3,
   cookiesPurchasedPerHour: [],
+  hoursInDayArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'], 
 
   numberOfCustomers: function () {
     var min = this.minCustomers;
@@ -16,8 +17,9 @@ var seattle = {
   findCookiesPerHour: function () {
     for (var i = 0; i < 14; i++) {
       var amountOfCookies = this.numberOfCustomers() * this.avgCookiePerCust;
-      this.cookiesPurchasedPerHour.push(Math.round(amountOfCookies));
+      var puttingInArray = this.cookiesPurchasedPerHour.push(Math.round(amountOfCookies));
     };
+    return puttingInArray;
   },
 
   findTotalCookies: function () {
@@ -30,31 +32,13 @@ var seattle = {
   },
 
   displaySales: function () {
-    for (var i = 0; i < this.cookiesPurchasedPerHour.length; i++){
+    for (var i = 0; i < this.cookiesPurchasedPerHour.length; i++) {
       var ulSeattleLi = document.getElementById('seattleStand');
       var liSeattle = document.createElement('li');
-      liSeattle.textContent = this.cookiesPurchasedPerHour[i];
+      liSeattle.textContent = this.hoursInDayArray[i] + ': ' + this.cookiesPurchasedPerHour[i] + ' cookies';
       ulSeattleLi.appendChild(liSeattle);
-    };
+    }
   },
-
-  
-
-
-  //   renderLikes: function(){
-//     for(var i = 0; i < this.thingsSheLikes.length; i++){
-//       // one time starts
-//       //1. find target
-//       var tofuLikesList = document.getElementById('tofu-likes');
-//       //2.
-//       var tofuLikesItem = document.createElement('li');
-//       // 2.5
-//       tofuLikesItem.textContent = this.thingsSheLikes[i];
-//       // 3.
-//       tofuLikesList.appendChild(tofuLikesItem);
-//     //one time ends
-//     }
-
 
 };
 
@@ -63,194 +47,96 @@ seattle.findCookiesPerHour();
 seattle.findTotalCookies();
 seattle.displaySales();
 
-// var tofu = {
-//   name: 'tofu',
-//   color: 'brown',
-//   pretty: true,
-//   age: 10,
-//   thingsSheLikes :['eating food', 'sniffing fresh air', 'loafing', 'be the alpha'],
-//   wakeUp: function () {
-//     // put an li on the page with details about snowdrop waking me up
-//     var parent = document.getElementById('alarms');
-//     var child = document.createElement('li');
-//     child.textContent = this.name + ' the ' + this.color + ' is ' + this.age + ' cat years old, wake up and give her food';
-//     parent.appendChild(child);
-//   },
-//   renderLikes: function(){
-//     for(var i = 0; i < this.thingsSheLikes.length; i++){
-//       // one time starts
-//       //1. find target
-//       var tofuLikesList = document.getElementById('tofu-likes');
-//       //2.
-//       var tofuLikesItem = document.createElement('li');
-//       // 2.5
-//       tofuLikesItem.textContent = this.thingsSheLikes[i];
-//       // 3.
-//       tofuLikesList.appendChild(tofuLikesItem);
-//     //one time ends
-//     }
+var tokyo = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookiePerCust: 1.2,
+  cookiesPurchasedPerHour: [],
+  hoursInDayArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'], 
 
-//   }
-// };
+  numberOfCustomers: function () {
+    var min = this.minCustomers;
+    var max = this.maxCustomers;
+    var randomNumber = Math.random() * (max - min) + min;
+    return Math.round(randomNumber);
+  },
 
-// tofu.wakeUp();
-// tofu.renderLikes();
+  findCookiesPerHour: function () {
+    for (var i = 0; i < 14; i++) {
+      var amountOfCookies = this.numberOfCustomers() * this.avgCookiePerCust;
+      var puttingInArray = this.cookiesPurchasedPerHour.push(Math.round(amountOfCookies));
+    };
+    return puttingInArray;
+  },
 
+  findTotalCookies: function () {
+    var totalCookies = 0;
+    for (var i = 0; i < this.cookiesPurchasedPerHour.length; i++) {
+      totalCookies += this.cookiesPurchasedPerHour[i];
+    };
+    this.cookiesPurchasedPerHour.push(totalCookies);
+    return totalCookies;
+  },
 
+  displaySales: function () {
+    for (var i = 0; i < this.cookiesPurchasedPerHour.length; i++) {
+      var ulTokyoLi = document.getElementById('tokyoStand');
+      var liTokyo = document.createElement('li');
+      liTokyo.textContent = this.hoursInDayArray[i] + ': ' + this.cookiesPurchasedPerHour[i] + ' cookies';
+      ulTokyoLi.appendChild(liTokyo);
+    }
+  },
 
+};
 
-// var tokyo = {
-//   minCustomers: 3,
-//   maxCustomers: 24,
-//   avgCookiePerCust: 1.2,
-//   numberOfCustomers: function () {
-//     return Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers
-//   },
-// };
+tokyo.numberOfCustomers();
+tokyo.findCookiesPerHour();
+tokyo.findTotalCookies();
+tokyo.displaySales();
 
-// var dubia = {
-//   minCustomers: 11,
-//   maxCustomers: 38,
-//   avgCookiePerCust: 3.7,
-//   numberOfCustomers: function () {
-//     return Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers
-//   },
-// };
+var dubai = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookiePerCust: 3.7,
+  cookiesPurchasedPerHour: [],
+  hoursInDayArray: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'], 
 
-// var paris = {
-//   minCustomers: 20,
-//   maxCustomers: 38,
-//   avgCookiePerCust: 2.3,
-//   numberOfCustomers: function () {
-//     return Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers
-//   },
-// };
+  numberOfCustomers: function () {
+    var min = this.minCustomers;
+    var max = this.maxCustomers;
+    var randomNumber = Math.random() * (max - min) + min;
+    return Math.round(randomNumber);
+  },
 
-// var lima = {
-//   minCustomers: 2,
-//   maxCustomers: 16,
-//   avgCookiePerCust: 4.6,
-//   numberOfCustomers: function () {
-//     return Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers
-//   },
-// };
+  findCookiesPerHour: function () {
+    for (var i = 0; i < 14; i++) {
+      var amountOfCookies = this.numberOfCustomers() * this.avgCookiePerCust;
+      var puttingInArray = this.cookiesPurchasedPerHour.push(Math.round(amountOfCookies));
+    };
+    return puttingInArray;
+  },
 
+  findTotalCookies: function () {
+    var totalCookies = 0;
+    for (var i = 0; i < this.cookiesPurchasedPerHour.length; i++) {
+      totalCookies += this.cookiesPurchasedPerHour[i];
+    };
+    this.cookiesPurchasedPerHour.push(totalCookies);
+    return totalCookies;
+  },
 
+  displaySales: function () {
+    for (var i = 0; i < this.cookiesPurchasedPerHour.length; i++) {
+      var ulDubaiLi = document.getElementById('dubaiStand');
+      var liDubai = document.createElement('li');
+      liDubai.textContent = this.hoursInDayArray[i] + ': ' + this.cookiesPurchasedPerHour[i] + ' cookies';
+      ulDubaiLi.appendChild(liDubai);
+    }
+  },
+  
+};
 
-
-// // ========== DOM Manipulation ================
-
-// /*
-//   1. find a target - find a element on the page that already exists for us to add to
-//     - we will use document.getElementById
-//   2. Create a new element - make a new element so it can exist
-//     2.5 - give the new element appropriate content
-//   3. put the new element on the parent - append it to the target
-//     - method of DOM ELEMENTS, `appendChild`
-// */
-
-// // put a p tag on the page
-
-// // 1. find a target - we needed to put an element with an id into the html file
-// var kittenStorage = document.getElementById('kittenPlace');
-// console.log(kittenStorage);
-// // 2. create element
-// var aNewPTag = document.createElement('p');
-// // 2.5 appropriate content - TEXT since it is a p tag
-// aNewPTag.textContent = 'snowdrop was here';
-// //3. put it on the parent
-// kittenStorage.appendChild(aNewPTag);
-
-
-// // add a new task to the list `feed the cat`
-// // 1. find the target
-// var taskList = document.getElementById('tasks');
-// // 2. create new element
-// var newTask = document.createElement('li');
-// //2.5 - content
-// newTask.textContent = 'feed the cat';
-// // 3. append to parent
-// taskList.appendChild(newTask);
-
-
-// var cats = ['avery', 'tofu', 'ragnar'];
-
-// for(var i = 0; i < 3; i++){
-
-//   // add a new task to the list `feed the cat`
-//   // 1. find the target
-//   var taskList = document.getElementById('tasks');
-//   // 2. create new element
-//   var newTask = document.createElement('li');
-//   //2.5 - content
-//   newTask.textContent = 'feed ' + cats[i];
-//   // 3. append to parent
-//   taskList.appendChild(newTask);
-// }
-
-
-// var snowdrop = {
-//   name: 'snowdrop',
-//   color: 'white',
-//   pretty: true,
-//   age : 12,
-//   wakeUp : function(){
-//     // put an li on the page with details about snowdrop waking me up
-//     var parent = document.getElementById('alarms');
-//     var child = document.createElement('li');
-//     child.textContent = this.name + ' the ' + this.color + ' is ' + this.age + ' cat years old, wake up and give her food';
-//     parent.appendChild(child);
-//   }
-// };
-
-// snowdrop.wakeUp();
-
-
-// var ragnar = {
-//   name: 'ragnar',
-//   color: 'black',
-//   pretty: true,
-//   age: 6,
-//   wakeUp: function () {
-//     // put an li on the page with details about snowdrop waking me up
-//     var parent = document.getElementById('alarms');
-//     var child = document.createElement('li');
-//     child.textContent = this.name + ' the ' + this.color + ' is ' + this.age + ' cat years old, wake up and give her food';
-//     parent.appendChild(child);
-//   }
-// };
-
-// ragnar.wakeUp();
-
-// var tofu = {
-//   name: 'tofu',
-//   color: 'brown',
-//   pretty: true,
-//   age: 10,
-//   thingsSheLikes :['eating food', 'sniffing fresh air', 'loafing', 'be the alpha'],
-//   wakeUp: function () {
-//     // put an li on the page with details about snowdrop waking me up
-//     var parent = document.getElementById('alarms');
-//     var child = document.createElement('li');
-//     child.textContent = this.name + ' the ' + this.color + ' is ' + this.age + ' cat years old, wake up and give her food';
-//     parent.appendChild(child);
-//   },
-//   renderLikes: function(){
-//     for(var i = 0; i < this.thingsSheLikes.length; i++){
-//       // one time starts
-//       //1. find target
-//       var tofuLikesList = document.getElementById('tofu-likes');
-//       //2.
-//       var tofuLikesItem = document.createElement('li');
-//       // 2.5
-//       tofuLikesItem.textContent = this.thingsSheLikes[i];
-//       // 3.
-//       tofuLikesList.appendChild(tofuLikesItem);
-//     //one time ends
-//     }
-
-//   }
-// };
-
-// tofu.wakeUp();
-// tofu.renderLikes();
+dubai.numberOfCustomers();
+dubai.findCookiesPerHour();
+dubai.findTotalCookies();
+dubai.displaySales();
